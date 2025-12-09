591885adb4504d84fd9fac4eb81b50a0a449f0b6
oRander <43866550+oRander@users.noreply.github.com>
2025-12-09 11:50:02 -0300

    fix: correcao import djangoAPI

--- src/components/Interno/Entregas/MotoboyDashboard.jsx
+++ src/components/Interno/Entregas/MotoboyDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import djangoApi from '../../api/djangoApi';
+import djangoApi from '../../../api/djangoApi';
+
 import dayjs from 'dayjs';
 
 export default function MotoboyDashboard({ user, onLogout }) {

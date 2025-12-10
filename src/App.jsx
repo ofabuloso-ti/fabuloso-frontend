15bd01fc0b5010e2d36d57d9c1ad7ad7b19dbf72
oRander <43866550+oRander@users.noreply.github.com>
2025-12-10 12:15:28 -0300

    fix: correcao import app atentend2e

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import {
 import djangoApi from './api/djangoApi';
 
 // Interno
-import DashboardAtendente from './components/Interno/Atendente/AtendenteDashboard';
-import AtendenteEntregas from './components/Interno/Atendente/AtendenteHeader';
+import DashboardAtendente from './components/Atendente/AtendenteDashboard';
+import AtendenteEntregas from './components/Atendente/AtendenteHeader';
 import FuncionarioDashboard from './components/Interno/FuncionarioDashboard';
 import AdminDashboard from './components/Interno/AdminDashboard';
 import RelatorioDiarioForm from './components/Interno/RelatorioDiarioForm';
